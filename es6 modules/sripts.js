cc17729703d67fde54fc8d6e7ea2ebdc5a139ace
Dennis Byberg <dennis.byberg@hotmail.com>
2022-11-21 10:51:37 +0100

    module calc

---
+++ es6 modules/sripts.js
@@ -0,0 +1,6 @@
+import * as calculator from "./calculator.js";
+
+console.log(calculator.add(5, 5));
+console.log(calculator.subtract(5, 5));
+console.log(calculator.divide(5, 5));
+console.log(calculator.multiply(5, 5));
