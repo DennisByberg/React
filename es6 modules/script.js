993783f484a5802ef0768d4a0004801377400872
Dennis Byberg <dennis.byberg@hotmail.com>
2022-11-22 11:08:18 +0100

    ...

---
+++ es6 modules/script.js
@@ -0,0 +1,9 @@
+import * as calculator from "./calculator.js";
+
+const number1 = 10;
+const number2 = 5;
+
+console.log(calculator.add(number1, number2));
+console.log(calculator.subtract(number1, number2));
+console.log(calculator.divide(number1, number2));
+console.log(calculator.multiply(number1, number2));
