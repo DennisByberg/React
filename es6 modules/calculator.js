aa88b6281a1e77ecffc0e4a72390707632f6f0e8
Dennis Byberg <dennis.byberg@hotmail.com>
2022-11-23 14:25:34 +0100

    -

--- es6 modules/calculator.js
+++ es6 modules/calculator.js
@@ -1,15 +1,20 @@
-export function add(number1, number2) {
+const number1 = 5;
+const number2 = 10;
+
+function add(number1, number2) {
   return number1 + number2;
 }
 
-export function subtract(number1, number2) {
+function subtract(number1, number2) {
   return number1 - number2;
 }
 
-export function divide(number1, number2) {
+function divide(number1, number2) {
   return number1 / number2;
 }
 
-export function multiply(number1, number2) {
+function multiply(number1, number2) {
   return number1 * number2;
 }
+
+export { add, subtract, divide, multiply, number1, number2 };
--- es6 modules/script.js
+++ es6 modules/script.js
@@ -1,7 +1,6 @@
 import * as calculator from "./calculator.js";
-
-const number1 = 10;
-const number2 = 5;
+const number1 = calculator.number1;
+const number2 = calculator.number2;
 
 console.log(calculator.add(number1, number2));
 console.log(calculator.subtract(number1, number2));
