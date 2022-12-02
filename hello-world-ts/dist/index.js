1f522f041d056935465bbd1480a42e28438723d0
Dennis Byberg <dennis.byberg@hotmail.com>
2022-12-02 18:41:22 +0100

    first ts

---
+++ hello-world-ts/dist/index.js
@@ -0,0 +1,7 @@
+"use strict";
+const age = 20;
+if (age > 50) {
+    console.log("hej");
+}
+console.log(age);
+//# sourceMappingURL=index.js.map
---
+++ hello-world-ts/src/index.ts
@@ -0,0 +1,7 @@
+const age: number = 20;
+
+if (age > 50) {
+  console.log("hej");
+}
+
+console.log(age);
