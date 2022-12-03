ee7d6f9d8276369a02e5be6ad3e79bc98c593dca
Dennis Byberg <dennis.byberg@hotmail.com>
2022-12-03 01:34:41 +0100

    ...

--- hello-world-ts/dist/index.js
+++ hello-world-ts/dist/index.js
@@ -1,6 +1,18 @@
 "use strict";
-const age = 20;
-if (age < 50) {
-    console.log("hej");
-}
+const employee1 = {
+    id: 1,
+    name: "Dennis",
+    retire: (date) => {
+        console.log(date);
+    },
+};
+const employee2 = {
+    id: 2,
+    name: "Erik",
+    retire: (date) => {
+        console.log(date);
+    },
+};
+console.log(employee1);
+console.log(employee2);
 //# sourceMappingURL=index.js.map
--- hello-world-ts/src/index.ts
+++ hello-world-ts/src/index.ts
@@ -1,6 +1,24 @@
-function calculateTax(income: number, taxYear: number): number {
-  if (taxYear < 2022) {
-    return income * 1.2;
-  }
-  return income * 1.3;
-}
+type Employee = {
+  readonly id: number;
+  name: string;
+  retire: (date: Date) => void;
+};
+
+const employee1: Employee = {
+  id: 1,
+  name: "Dennis",
+  retire: (date: Date) => {
+    console.log(date);
+  },
+};
+
+const employee2: Employee = {
+  id: 2,
+  name: "Erik",
+  retire: (date: Date) => {
+    console.log(date);
+  },
+};
+
+console.log(employee1);
+console.log(employee2);
