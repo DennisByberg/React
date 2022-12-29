3b05ee5623008387fabdb5dd946e3b8d26e63433
Dennis Byberg <dennis.byberg@hotmail.com>
2022-12-30 00:46:06 +0100

    ...

--- react-tutorial-v1/src/Content.jsx
+++ react-tutorial-v1/src/Content.jsx
@@ -1,9 +1,24 @@
 import React from "react";
 
 const Content = () => {
+  const handleClick = () => {
+    console.log("You Clicked It!");
+  };
+
+  const handleClick2 = (name) => {
+    console.log(`${name} was clicked`);
+  };
+
+  const handleClick3 = (e) => {
+    console.log(e.target.innerText);
+  };
+
   return (
     <main>
       <h2>Hello</h2>
+      <button onClick={handleClick}>Click Me</button>
+      <button onClick={() => handleClick2("Dennis")}>Click Me 2</button>
+      <button onClick={(e) => handleClick3(e)}>Click Me 3</button>
     </main>
   );
 };
