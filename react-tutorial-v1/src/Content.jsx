fb17cba1783c887ff0b91025414f3b423af13e7c
Dennis Byberg <dennis.byberg@hotmail.com>
2023-01-04 19:11:15 +0100

    ...

--- react-tutorial-v1/src/Content.jsx
+++ react-tutorial-v1/src/Content.jsx
@@ -1,36 +1,20 @@
 import React from "react";
-import { useState } from "react";
+import ItemList from "./ItemList";
 
-const Content = () => {
-  const [name, setName] = useState("Dennis");
-  const [count, setCount] = useState(0);
-
-  const handleNameChange = () => {
-    const names = ["Dennis", "Kevin", "Erik"];
-    const int = Math.floor(Math.random() * 3);
-    setName(names[int]);
-  };
-
-  const handleClick = () => {
-    setCount(count + 1);
-    console.log(count);
-  };
-
-  const handleClick2 = () => {
-    console.log(count);
-  };
-
-  const handleClick3 = (e) => {
-    console.log(e.target.innerText);
-  };
-
+const Content = ({ items, handleCheck, handleDelete }) => {
   return (
     <main>
-      <h2>Hello</h2>
-      <p onDoubleClick={handleClick}>Hello {name}!</p>
-      <button onClick={handleNameChange}>Change Name</button>
-      <button onClick={handleClick}>Counter</button>
-      <button onClick={handleClick2}>Click Me 2</button>
+      {items.length ? (
+        <ItemList
+          items={items}
+          handleCheck={handleCheck}
+          handleDelete={handleDelete}
+        />
+      ) : (
+        <p style={{ color: "white", fontSize: "2rem", fontWeight: "bold" }}>
+          NO ITEMS IN THE LIST
+        </p>
+      )}
     </main>
   );
 };
