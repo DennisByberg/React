6b560c4957819ea0900f8dce631336f414746490
Dennis Byberg <dennis.byberg@hotmail.com>
2023-01-06 19:13:21 +0100

    ...

--- random-horse/src/components/HorsePictureGallery/HorsePictureGallery.jsx
+++ random-horse/src/components/HorsePictureGallery/HorsePictureGallery.jsx
@@ -3,8 +3,45 @@ import HorsePictureGalleryCSS from "../HorsePictureGallery/HorsePictureGallery.m
 
 const HorsePictureGallery = () => {
   return (
-    <div id="gallery">
-      <p>Hej</p>
+    <div className={HorsePictureGalleryCSS.galleryContainer}>
+      <ul>
+        <li>
+          <img
+            src="https://images.unsplash.com/photo-1573751056139-2ab65b6b03be?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=612&q=80"
+            alt="picture of a horse"
+          />
+        </li>
+        <li>
+          <img
+            src="https://images.unsplash.com/photo-1573751056139-2ab65b6b03be?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=612&q=80"
+            alt="picture of a horse"
+          />
+        </li>
+        <li>
+          <img
+            src="https://images.unsplash.com/photo-1573751056139-2ab65b6b03be?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=612&q=80"
+            alt="picture of a horse"
+          />
+        </li>
+        <li>
+          <img
+            src="https://images.unsplash.com/photo-1573751056139-2ab65b6b03be?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=612&q=80"
+            alt="picture of a horse"
+          />
+        </li>
+        <li>
+          <img
+            src="https://images.unsplash.com/photo-1573751056139-2ab65b6b03be?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=612&q=80"
+            alt="picture of a horse"
+          />
+        </li>
+        <li>
+          <img
+            src="https://images.unsplash.com/photo-1573751056139-2ab65b6b03be?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=612&q=80"
+            alt="picture of a horse"
+          />
+        </li>
+      </ul>
     </div>
   );
 };
