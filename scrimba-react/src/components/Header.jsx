978b3de32edb0c1c995181c856db5e3fc184e83e
Dennis Byberg <dennis.byberg@hotmail.com>
2023-01-13 15:00:28 +0100

    ...

---
+++ scrimba-react/src/components/Header.jsx
@@ -0,0 +1,15 @@
+const Header = () => {
+  return (
+    <header>
+      <img
+        src="https://cdn4.iconfinder.com/data/icons/logos-3/600/React.js_logo-512.png"
+        alt="react logo"
+        width="150px"
+      />
+      <p className="logoName">ReactFacts</p>
+      <p className="headerText">React Course - Project 1</p>
+    </header>
+  );
+};
+
+export default Header;
