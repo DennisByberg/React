481e331d51e5eda5d2347cdff55d661279ee3d29
Dennis Byberg <dennis.byberg@hotmail.com>
2023-03-09 13:06:15 +0100

    updated with state

--- shakespeare-react/src/App.jsx
+++ shakespeare-react/src/App.jsx
@@ -1,10 +1,24 @@
 import "./App.css";
 import Insult from "./components/Insult/Insult";
-import insultData from "../src/insultData";
 import AddInsult from "./components/AddInsult/AddInsult";
+import { useState } from "react";
 
 function App() {
-  const insults = insultData.map((insult) => {
+  const [insults, setInsults] = useState([
+    {
+      id: 1,
+      insultText:
+        "Were such things here as we do speak about? Or have we eaten on the insane root That takes the reason prisoner?",
+      play: "Macbeth",
+    },
+    {
+      id: 2,
+      insultText: "Never hung poison on a fouler toad",
+      play: "Richard III",
+    },
+  ]);
+
+  const insultsComponent = insults.map((insult) => {
     return (
       <Insult
         key={insult.id}
@@ -21,15 +35,21 @@ function App() {
       play: newPlay,
     };
 
-    insults.push(newInsultObject);
-    console.log(insults);
+    // create a copy of insults array using spread operator
+    const temp = [...insults];
+
+    // add a new insult object to the temp array
+    temp.push(newInsultObject);
+
+    // update the state of Insults using setInsults method by passing temp array as argument
+    setInsults(temp);
   }
 
   return (
     <div className="App">
       <h1>Shakespeare Insults</h1>
       <AddInsult addNewInsult={addNewInsult} />
-      <ul>{insults}</ul>
+      <ul>{insultsComponent}</ul>
     </div>
   );
 }
