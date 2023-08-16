6243c445a9d1d8906a525503540e032fddedab1d
Dennis Byberg <dennis.byberg@hotmail.com>
2023-08-16 03:29:56 +0200

    pillpill...

--- pendlaren-skiss/src/App.tsx
+++ pendlaren-skiss/src/App.tsx
@@ -1,7 +1,13 @@
 import "./App.scss";
+import Stops from "./components/Stops/Stops";
 
 function App() {
-  return <div className="App">APP</div>;
+  return (
+    <div className="App">
+      <header className="App__header"> Pendlaren </header>
+      <Stops />
+    </div>
+  );
 }
 
 export default App;
---
+++ pendlaren-skiss/src/components/StopCard/StopCard.tsx
@@ -0,0 +1,29 @@
+// SCSS.
+import "./StopCard.scss";
+
+function StopCard({ nameOfStop, extId }) {
+  const API_KEY = "73f1d3b1-8698-4efb-94d9-f2fb1704bf06";
+
+  async function getPoleTimeTables(extId: string) {
+    console.log(extId);
+    console.log(API_KEY);
+    const URL = `https://api.resrobot.se/v2.1/departureBoard?id=${extId}&format=json&accessId=${API_KEY}`;
+    const response = await fetch(URL);
+
+    const data = await response.json();
+    console.log(data);
+  }
+
+  return (
+    <section className="stop-card">
+      <h1
+        onClick={() => getPoleTimeTables(extId)}
+        className="stop-card__name-of-stop"
+      >
+        {nameOfStop}
+      </h1>
+    </section>
+  );
+}
+
+export default StopCard;
